5bebd33d3cbc1468c35a3147d45583c32ada2efc
danny <jskim@triphos.co.kr>
2022-07-01 21:46:19 +0900

    axios, swal 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Swal from 'vue-sweetalert2';
 Vue.config.productionTip = false
+import {
+  $get, $post, $loading, $hideLoading, $converter, $error, $info, $confirm, $question, $success, $warning, $makeSwal
+} from "@/script/common";
+import MemberInfo from "@/script/memberInfo";
 
-new Vue({
+/** CSS */
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const app = new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+app.use(Swal);
+/* Common_Func */
+app.config.globalProperties.$get = $get;
+app.config.globalProperties.$post = $post;
+app.config.globalProperties.$loading = $loading;
+app.config.globalProperties.$hideLoading = $hideLoading;
+app.config.globalProperties.$converter = $converter;
+app.config.globalProperties.$error = $error;
+app.config.globalProperties.$info = $info
+app.config.globalProperties.$confirm = $confirm;
+app.config.globalProperties.$question = $question;
+app.config.globalProperties.$success = $success;
+app.config.globalProperties.$warning = $warning;
+app.config.globalProperties.$makeSwal = $makeSwal;
+app.config.globalProperties.$MemberInfo = new MemberInfo.MemberInfo(app);
